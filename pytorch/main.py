f944f1a78dca00599af96c4a835b3dd4f173d888
KimDongHa00 <dhk8110@gmail.com>
2025-04-03 00:23:47 +0900

    Update main.py

--- pytorch/main.py
+++ pytorch/main.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-@Author: Yue Wang
-@Contact: yuewangx@mit.edu
-@File: main.py
-@Time: 2018/10/13 10:39 PM
-"""
-
-
 from __future__ import print_function
 import os
 import argparse
@@ -16,234 +6,161 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from data import ModelNet40
-from model import PointNet, DGCNN
-import numpy as np
-from torch.utils.data import DataLoader
+from data import ModelNet40_Merged
+from model import DGCNN_PN_Merge
 from util import cal_loss, IOStream
+import numpy as np
 import sklearn.metrics as metrics
-
+from torch.utils.data import DataLoader
 
 def _init_():
     if not os.path.exists('checkpoints'):
         os.makedirs('checkpoints')
-    if not os.path.exists('checkpoints/'+args.exp_name):
-        os.makedirs('checkpoints/'+args.exp_name)
-    if not os.path.exists('checkpoints/'+args.exp_name+'/'+'models'):
-        os.makedirs('checkpoints/'+args.exp_name+'/'+'models')
-    os.system('cp main.py checkpoints'+'/'+args.exp_name+'/'+'main.py.backup')
-    os.system('cp model.py checkpoints' + '/' + args.exp_name + '/' + 'model.py.backup')
-    os.system('cp util.py checkpoints' + '/' + args.exp_name + '/' + 'util.py.backup')
-    os.system('cp data.py checkpoints' + '/' + args.exp_name + '/' + 'data.py.backup')
+    if not os.path.exists(f'checkpoints/{args.exp_name}'):
+        os.makedirs(f'checkpoints/{args.exp_name}')
+    if not os.path.exists(f'checkpoints/{args.exp_name}/models'):
+        os.makedirs(f'checkpoints/{args.exp_name}/models')
+    os.system(f'cp main.py checkpoints/{args.exp_name}/main.py.backup')
+    os.system(f'cp model.py checkpoints/{args.exp_name}/model.py.backup')
+    os.system(f'cp util.py checkpoints/{args.exp_name}/util.py.backup')
+    os.system(f'cp data.py checkpoints/{args.exp_name}/data.py.backup')
 
 def train(args, io):
-    train_loader = DataLoader(ModelNet40(partition='train', num_points=args.num_points, datamodel=args.datamodel ), num_workers=8,
-                              batch_size=args.batch_size, shuffle=True, drop_last=True)
-    test_loader = DataLoader(ModelNet40(partition='test', num_points=args.num_points, datamodel=args.datamodel), num_workers=8,
-                             batch_size=args.test_batch_size, shuffle=True, drop_last=False)
+    train_loader = DataLoader(ModelNet40_Merged(partition='train', num_points=args.num_points),
+                              num_workers=8, batch_size=args.batch_size, shuffle=True, drop_last=True)
+    test_loader = DataLoader(ModelNet40_Merged(partition='test', num_points=args.num_points),
+                             num_workers=8, batch_size=args.test_batch_size, shuffle=False, drop_last=False)
 
     device = torch.device("cuda" if args.cuda else "cpu")
-
-    #Try to load models
-    if args.model == 'pointnet':
-        model = PointNet(args).to(device)
-    elif args.model == 'dgcnn':
-        datamodel_dims = {
-            'xyz': 3,
-            'full': 59,
-            'Op': 4,   # XYZ + Opacity (3 + 1)
-            'Sh': 51,  # XYZ + SH Coeffs (3 + 48)
-            'SR': 10,  # XYZ + Scale + Rotation (3 + 3 + 4)
-            'ECSH': 11 # XYZ + Opacity + Scale + Rotation (3 + 1 + 3 + 4)
-        }	  
-        input_dim = datamodel_dims.get(args.datamodel, 3)  # 기본값 3(XYZ)
-        model = DGCNN(args, input_dim=input_dim).to(device)
-
-    else:
-        raise Exception("Not implemented")
-    print(str(model))
+    model = DGCNN_PN_Merge(args).to(device)
+    io.cprint(str(model))
 
     model = nn.DataParallel(model)
-    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    io.cprint(f"Using {torch.cuda.device_count()} GPUs")
 
-    if args.use_sgd:
-        print("Use SGD")
-        opt = optim.SGD(model.parameters(), lr=args.lr*100, momentum=args.momentum, weight_decay=1e-4)
-    else:
-        print("Use Adam")
-        opt = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
-
+    opt = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
     scheduler = CosineAnnealingLR(opt, args.epochs, eta_min=args.lr)
-    
     criterion = cal_loss
 
     best_test_acc = 0
+
     for epoch in range(args.epochs):
         scheduler.step()
-        ####################
-        # Train
-        ####################
+
+        model.train()
         train_loss = 0.0
         count = 0.0
-        model.train()
         train_pred = []
         train_true = []
-        for data, label in train_loader:
-            data, label = data.to(device), label.to(device).squeeze()
-            data = data.permute(0, 2, 1)
-            batch_size = data.size()[0]
+
+        for csh, osr, label in train_loader:
+            csh, osr, label = csh.to(device), osr.to(device), label.to(device).squeeze()
+            csh = csh.permute(0, 2, 1)
+            osr = osr.permute(0, 2, 1)
+
             opt.zero_grad()
-            logits = model(data)
+            logits = model(csh, osr)
             loss = criterion(logits, label)
             loss.backward()
             opt.step()
+
             preds = logits.max(dim=1)[1]
-            count += batch_size
-            train_loss += loss.item() * batch_size
+            count += csh.size(0)
+            train_loss += loss.item() * csh.size(0)
             train_true.append(label.cpu().numpy())
             train_pred.append(preds.detach().cpu().numpy())
+
         train_true = np.concatenate(train_true)
         train_pred = np.concatenate(train_pred)
-        outstr = 'Train %d, loss: %.6f, train acc: %.6f, train avg acc: %.6f' % (epoch,
-                                                                                 train_loss*1.0/count,
-                                                                                 metrics.accuracy_score(
-                                                                                     train_true, train_pred),
-                                                                                 metrics.balanced_accuracy_score(
-                                                                                     train_true, train_pred))
-        io.cprint(outstr)
+        io.cprint(f"Train {epoch}, loss: {train_loss/count:.6f}, acc: {metrics.accuracy_score(train_true, train_pred):.6f}, avg acc: {metrics.balanced_accuracy_score(train_true, train_pred):.6f}")
 
-        ####################
-        # Test
-        ####################
+        # Evaluation
+        model.eval()
         test_loss = 0.0
         count = 0.0
-        model.eval()
         test_pred = []
         test_true = []
-        for data, label in test_loader:
-            data, label = data.to(device), label.to(device).squeeze()
-            data = data.permute(0, 2, 1)
-            batch_size = data.size()[0]
-            logits = model(data)
+
+        for csh, osr, label in test_loader:
+            csh, osr, label = csh.to(device), osr.to(device), label.to(device).squeeze()
+            csh = csh.permute(0, 2, 1)
+            osr = osr.permute(0, 2, 1)
+
+            logits = model(csh, osr)
             loss = criterion(logits, label)
+
             preds = logits.max(dim=1)[1]
-            count += batch_size
-            test_loss += loss.item() * batch_size
+            count += csh.size(0)
+            test_loss += loss.item() * csh.size(0)
             test_true.append(label.cpu().numpy())
             test_pred.append(preds.detach().cpu().numpy())
+
         test_true = np.concatenate(test_true)
         test_pred = np.concatenate(test_pred)
         test_acc = metrics.accuracy_score(test_true, test_pred)
         avg_per_class_acc = metrics.balanced_accuracy_score(test_true, test_pred)
-        outstr = 'Test %d, loss: %.6f, test acc: %.6f, test avg acc: %.6f' % (epoch,
-                                                                              test_loss*1.0/count,
-                                                                              test_acc,
-                                                                              avg_per_class_acc)
-        io.cprint(outstr)
+        io.cprint(f"Test {epoch}, loss: {test_loss/count:.6f}, acc: {test_acc:.6f}, avg acc: {avg_per_class_acc:.6f}")
+
         if test_acc >= best_test_acc:
             best_test_acc = test_acc
-            torch.save(model.state_dict(), 'checkpoints/%s/models/model.t7' % args.exp_name)
-
+            torch.save(model.state_dict(), f'checkpoints/{args.exp_name}/models/model.t7')
 
 def test(args, io):
-    test_loader = DataLoader(ModelNet40(partition='test', num_points=args.num_points, datamodel=args.datamodel),
-                             batch_size=args.test_batch_size, shuffle=True, drop_last=False)
+    test_loader = DataLoader(ModelNet40_Merged(partition='test', num_points=args.num_points),
+                             batch_size=args.test_batch_size, shuffle=False, drop_last=False)
 
     device = torch.device("cuda" if args.cuda else "cpu")
-
-    # datamodel에 따라 input_dim 설정 추가 (train과 동일하게)
-    datamodel_dims = {
-        'xyz': 3,
-        'full': 59,
-        'Op': 4,   # XYZ + Opacity (3 + 1)
-        'Sh': 51,  # XYZ + SH Coeffs (3 + 48)
-        'SR': 10,  # XYZ + Scale + Rotation (3 + 3 + 4)
-        'ECSH': 11 # XYZ + Opacity + Scale + Rotation (3 + 1 + 3 + 4)
-    }
-    input_dim = datamodel_dims.get(args.datamodel, 3)  # 기본값 3(XYZ)
-    
-    # input_dim을 DGCNN에 전달
-    model = DGCNN(args, input_dim=input_dim).to(device)
-    
+    model = DGCNN_PN_Merge(args).to(device)
     model = nn.DataParallel(model)
     model.load_state_dict(torch.load(args.model_path))
-    model = model.eval()
-    test_acc = 0.0
-    count = 0.0
-    test_true = []
-    test_pred = []
-    for data, label in test_loader:
+    model.eval()
 
-        data, label = data.to(device), label.to(device).squeeze()
-        data = data.permute(0, 2, 1)
-        batch_size = data.size()[0]
-        logits = model(data)
+    test_pred = []
+    test_true = []
+
+    for csh, osr, label in test_loader:
+        csh, osr, label = csh.to(device), osr.to(device), label.to(device).squeeze()
+        csh = csh.permute(0, 2, 1)
+        osr = osr.permute(0, 2, 1)
+        logits = model(csh, osr)
         preds = logits.max(dim=1)[1]
         test_true.append(label.cpu().numpy())
         test_pred.append(preds.detach().cpu().numpy())
+
     test_true = np.concatenate(test_true)
     test_pred = np.concatenate(test_pred)
     test_acc = metrics.accuracy_score(test_true, test_pred)
     avg_per_class_acc = metrics.balanced_accuracy_score(test_true, test_pred)
-    outstr = 'Test :: test acc: %.6f, test avg acc: %.6f'%(test_acc, avg_per_class_acc)
-    io.cprint(outstr)
-
+    io.cprint(f'Test :: acc: {test_acc:.6f}, avg acc: {avg_per_class_acc:.6f}')
 
 if __name__ == "__main__":
-    # Training settings
-    parser = argparse.ArgumentParser(description='Point Cloud Recognition')
-    parser.add_argument('--exp_name', type=str, default='exp', metavar='N',
-                        help='Name of the experiment')
-    parser.add_argument('--model', type=str, default='dgcnn', metavar='N',
-                        choices=['pointnet', 'dgcnn'],
-                        help='Model to use, [pointnet, dgcnn]')
-    parser.add_argument('--dataset', type=str, default='modelnet40', metavar='N',
-                        choices=['modelnet40'])
-    parser.add_argument('--batch_size', type=int, default=32, metavar='batch_size',
-                        help='Size of batch)')
-    parser.add_argument('--test_batch_size', type=int, default=16, metavar='batch_size',
-                        help='Size of batch)')
-    parser.add_argument('--epochs', type=int, default=250, metavar='N',
-                        help='number of episode to train ')
-    parser.add_argument('--use_sgd', type=bool, default=True,
-                        help='Use SGD')
-    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
-                        help='learning rate (default: 0.001, 0.1 if using sgd)')
-    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
-                        help='SGD momentum (default: 0.9)')
-    parser.add_argument('--no_cuda', type=bool, default=False,
-                        help='enables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--eval', type=bool,  default=False,
-                        help='evaluate the model')
-    parser.add_argument('--num_points', type=int, default=1024,
-                        help='num of points to use')
-    parser.add_argument('--dropout', type=float, default=0.5,
-                        help='dropout rate')
-    parser.add_argument('--emb_dims', type=int, default=1024, metavar='N',
-                        help='Dimension of embeddings')
-    parser.add_argument('--k', type=int, default=20, metavar='N',
-                        help='Num of nearest neighbors to use')
-    parser.add_argument('--model_path', type=str, default='', metavar='N',
-                        help='Pretrained model path')
-    #추가한 부분: datamodel 옵션 추가                  
-    parser.add_argument('--datamodel', type=str, default='xyz', 
-                    choices=['xyz', 'full', 'Op', 'Sh', 'SR', 'ECSH'],
-                    help='Data model mode: xyz, full, Op (Opacity), Sh (SH Coeffs), SR (Scale+Rotation), ECSH (Opacity+Scale+Rotation)')
-                    
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Point Cloud Recognition with DGCNN + PointNet Merge')
+    parser.add_argument('--exp_name', type=str, default='exp_merge', help='Experiment name')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--test_batch_size', type=int, default=16)
+    parser.add_argument('--epochs', type=int, default=250)
+    parser.add_argument('--use_sgd', type=bool, default=False)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--no_cuda', type=bool, default=False)
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--eval', type=bool, default=False)
+    parser.add_argument('--num_points', type=int, default=1024)
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--emb_dims', type=int, default=1024)
+    parser.add_argument('--k', type=int, default=20)
+    parser.add_argument('--model_path', type=str, default='')
 
+    args = parser.parse_args()
     _init_()
 
-    io = IOStream('checkpoints/' + args.exp_name + '/run.log')
+    io = IOStream(f'checkpoints/{args.exp_name}/run.log')
     io.cprint(str(args))
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
     torch.manual_seed(args.seed)
     if args.cuda:
-        io.cprint(
-            'Using GPU : ' + str(torch.cuda.current_device()) + ' from ' + str(torch.cuda.device_count()) + ' devices')
+        io.cprint(f'Using GPU : {torch.cuda.current_device()} from {torch.cuda.device_count()} devices')
         torch.cuda.manual_seed(args.seed)
     else:
         io.cprint('Using CPU')
@@ -252,3 +169,5 @@ if __name__ == "__main__":
         train(args, io)
     else:
         test(args, io)
+
+
